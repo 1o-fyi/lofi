0eca66c25b279f835c415717de0ba4fa27f1f5e8
John S <john@sahhar.io>
2021-11-10 23:58:00 -0800

    all: client side bls
    Signed-off-by: John S <john@sahhar.io>

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/1o-fyi/lofi/cmd"
 )
 
 func main() {
-	cmd.Execute()
+	if err := cmd.RootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
